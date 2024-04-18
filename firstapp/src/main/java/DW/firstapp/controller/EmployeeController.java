d92bd97e93ce712c1feb0a9181df4de6f9042ba4
SeungHwanYuk <ysh54244902@gmail.com>
2024-04-18 14:06:52 +0900

    0418

--- firstapp/src/main/java/DW/firstapp/controller/EmployeeController.java
+++ firstapp/src/main/java/DW/firstapp/controller/EmployeeController.java
@@ -3,19 +3,48 @@ package DW.firstapp.controller;
 import DW.firstapp.model.Employee;
 import DW.firstapp.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class EmployeeController {
 
-    // 의존성 주입
-    @Autowired
+    // @Autowired  // 의존성 주입 (필드 주입)
+    // 각 필드마다 @Autowired를 달아줘야한다
     EmployeeService employeeService;
+
+    // 매개변수를 사용한 생성자
+    @Autowired // 생성자 주입
+    // 생성자 하나에 각 필드를 한꺼번에 묶어서 @Autowired는 한번만 선언해도 된다
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
     @PostMapping("/api/employee")
     public Employee saveEmployee(@RequestBody Employee employee) {
         // service 코드 위치
         return employeeService.saveEmployee(employee);
+    }
+
+    @GetMapping("api/employee")
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+
+    @GetMapping("api/employee/{id}")
+    public Employee getEmployeeById(@PathVariable long id) {
+        return employeeService.getEmployeeById(id);
+    }
+
+    @PutMapping("api/employee/{id}")
+    public Employee updateEmployeeById(@PathVariable long id,
+                                       @RequestBody Employee employee) {
+        return employeeService.updateEmployeeById(id, employee);
+    }
+
+    @DeleteMapping("api/employee/{id}")
+    public Employee deleteEmployeeById(@PathVariable long id) {
+        return employeeService.deleteEmployeeById(id);
     }
 }
--- firstapp/src/main/java/DW/firstapp/service/EmployeeService.java
+++ firstapp/src/main/java/DW/firstapp/service/EmployeeService.java
@@ -2,8 +2,14 @@ package DW.firstapp.service;
 
 import DW.firstapp.model.Employee;
 import DW.firstapp.repository.EmployeeRepository;
+import jakarta.persistence.Id;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeService {
@@ -14,5 +20,47 @@ public class EmployeeService {
         // repository 코드 위치 (save)
         employeeRepository.save(employee);
         return employee;
+    }
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee getEmployeeById(long id) {
+        // ID로 해당 데이터 찾기
+        Optional<Employee> employee = employeeRepository.findById(id);
+        if (employee.isEmpty()){
+            // 예외처리
+            return null;
+        } else {
+            return employee.get();
+        }
+    }
+
+    public Employee updateEmployeeById(long id,Employee employee) {
+      Optional<Employee> employee1 = employeeRepository.findById(id);
+      if (employee1.isPresent()) {
+          // 데이터 업데이트
+          employee1.get().setEmail(employee.getEmail());
+          employee1.get().setFirstName(employee.getFirstName());
+          employee1.get().setLastName(employee.getLastName());
+          // 실제로 DB에 저장하기
+          employeeRepository.save(employee1.get());
+
+          return employee1.get();
+      } else {
+          return null;
+      }
+    }
+
+
+    public Employee deleteEmployeeById(long id) {
+        Optional<Employee> employee = employeeRepository.findById(id);
+        if (employee.isPresent()) {
+            employeeRepository.deleteById(id);
+            return employee.get();
+        } else {
+            return null;
+        }
     }
 }
